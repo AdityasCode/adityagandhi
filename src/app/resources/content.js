5f7d1dda03a9918d95b465d9ebd9c19348161e70
Aditya Gandhi <adityagandhi98101@gmail.com>
2025-05-25 13:04:28 +0530

    Updating status user-side.

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -54,7 +54,7 @@ const home = {
   label: "Home",
   title: `${person.name}'s Portfolio`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
-  headline: <>Building bridges between design and code</>,
+  headline: <>[Website currently in development!]</>,
   featured: {
     display: true,
     title: <>Recent project: <strong className="ml-4">Once UI</strong></>,
